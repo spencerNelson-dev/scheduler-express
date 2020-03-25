c1cf41de65faabd322aa4138fb3a6759a67faaf2
Spencer Nelson <spencernelson144@gmail.com>
2020-03-25 16:36:30 -0600

    added cors

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ db.connect(app.locals)
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
 
+    app.use(cors())
     app.use(logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));

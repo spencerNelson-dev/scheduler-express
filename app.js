42378a5aaa1a1e0c1d2ce8ac2b61694c73763061
Spencer Nelson <spencernelson144@gmail.com>
2020-03-26 15:49:53 -0600

    bug fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
     app.use(express.static(path.join(__dirname, 'public')));
 
     //app.use('/', indexRouter);
-    app.use('/users', usersRouter);
+    //app.use('/users', usersRouter);
     app.use('/events', eventsRouter)
 
     // catch 404 and forward to error handler

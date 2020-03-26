e897e995d4cbe54b122a47971ec59605e4dd9c64
Spencer Nelson <spencernelson144@gmail.com>
2020-03-26 15:24:42 -0600

    added react build files

---
+++ public/precache-manifest.f6f329015a1ec9924aea80bb8daa3fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffe0ab83c003c1528d81cf62300e43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdef9eadb72a2b33d6b1",
+    "url": "/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "2a3241a03205dfd7223c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cdef9eadb72a2b33d6b1",
+    "url": "/static/js/2.14200b89.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.14200b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3241a03205dfd7223c",
+    "url": "/static/js/main.fde67e89.chunk.js"
+  },
+  {
+    "revision": "2dfadd9f37b83d3158d3",
+    "url": "/static/js/runtime-main.a1d14dbd.js"
+  }
+]);
